7bfbe0c9ddd95311f999f045e62c669e7a4ff4c9
jawhitelaw <jwhitela@macalester.edu>
2022-04-25 13:44:34 -0500

    initial commit

---
+++ Intern Test Script.R
@@ -0,0 +1,27 @@
+#Hi! Thanks for applying to be a Data Science intern for the New York Red Bulls!
+
+#In the latest email I sent you were presented a csv of anonymized player 
+#   game by game information. For this task, you will be asked 
+#   the following question:
+#
+#   Question: "Please Identify your list of the top 5 players in this dataset 
+#    "that fit the Red Bull philosophy and explain your methodolgy and why you selected them."
+
+# All of the relevant packages you will need to complete this exercise are below.
+
+#Note: Feel free to present the information in any way you see fit, but please
+  #include all code. We must be able to run your code and your code must 
+  #be reproducible in order to be considered for this role. 
+
+###Libraries#############
+
+library(tidyverse)
+library(readr)
+
+###Code#############
+
+
+
+
+
+
